1de220fe363720d4d7e6a011759aeb6621d17f61
Tawhid-sw <tawhidsarkar684@gnail.com>
2026-02-05 12:31:06 +0600

    fix: Component naming error

--- src/routes/auth/signup/index.tsx
+++ src/routes/auth/signup/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { SignUpForm } from "@/components/signup-form";
+import { SignUpForm } from "@/components/Signup-form";
 
 export const Route = createFileRoute("/auth/signup/")({
   component: RouteComponent,

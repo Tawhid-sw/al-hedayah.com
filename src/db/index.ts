477d68d41969d153e1ad03d9b5828ea534f9dd7e
Tawhid-sw <tawhidsarkar684@gnail.com>
2026-02-06 11:10:12 +0600

    change: server change

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,13 @@
 import { drizzle } from "drizzle-orm/neon-serverless";
 import * as schema from "./auth-schema";
-import { serverEnv } from "@/lib/server-env";
+// import { serverEnv } from "@/lib/server-env";
 import { Pool } from "@neondatabase/serverless";
 
-if (!serverEnv.DATABASE_URL) throw new Error("DATABASE_URL is not set");
+if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
 const pool = new Pool({
-  connectionString: serverEnv.DATABASE_URL,
-  ssl: serverEnv.DATABASE_URL?.includes("neondb")
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes("neondb")
     ? {
         rejectUnauthorized: false,
       }
